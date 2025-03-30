bc7ff31b20a0c117e0918c7e566d60685de291af
Rishabh Sikarwar <rishabhsikarwar1000@gmail.com>
2025-03-30 22:38:19 +0530

    image path from import

--- src/pages/Page6.jsx
+++ src/pages/Page6.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import robot from '../assets/Robot.mp4'
+import react from '../assets/react.png'
 
 const page6 = () => {
   return (
     <div className='bg-emerald-400 flex items-center justify-center '>
-      <video src="/src/assets/Robot.mp4" autoPlay loop muted className='object-cover relative'></video>
-      <img src="/src/assets/react.png" alt=""  className='absolute object-cover h-[100vh]'/>
+      <video src={robot} autoPlay loop muted className='object-cover relative'></video>
+      <img src={react} alt=""  className='absolute object-cover h-[100vh]'/>
     </div>
   )
 }
